8bd11af953dc557e814f3807e05c7f90714c4e9e
Fernando <fernando-perugachi@hotmail.com>
2022-11-02 23:19:45 -0500

    Add all folders in src

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,7 +1,54 @@
 const Users = require('./users.models')
+const Categories = require('./categories.models')
+const Ingredients = require('./ingredients.models')
+const Instructions = require('./instructions.models')
+const RecipesIngredients = require('./recipes_ingredients.models')
+const Recipes = require('./recipes.models')
+const Types = require('./types.models')
+const UsersIngredients = require('./users_ingredients.models')
+const UsersRecipes = require('./users_recipes.models')
+
 
 const initModels = () => {
+    //* 1:M -> Users - Recipes 
+    Users.hasMany(Recipes)
+    Recipes.belongsTo(Users)
 
+    //* 1:M -> Users - UsersRecipes
+    Users.hasMany(UsersRecipes)
+    UsersRecipes.belongsTo(Users)
+
+    //* 1:M -> Recipes - UsersRecipes
+    Recipes.hasMany(UsersRecipes)
+    UsersRecipes.belongsTo(Recipes)
+
+    //* 1:M -> Categories - Recipes
+    Categories.hasMany(Recipes)
+    Recipes.belongsTo(Categories)
+
+    //* 1:M -> Types - Ingredients
+    Types.hasMany(Ingredients)
+    Ingredients.belongsTo(Recipes)
+
+    //* 1:M -> Recipes - RecipesIngredients
+    Recipes.hasMany(RecipesIngredients)
+    RecipesIngredients.belongsTo(Recipes)
+
+    //* 1:M -> Ingredients - RecipesIngredients
+    Ingredients.hasMany(RecipesIngredients)
+    RecipesIngredients.belongsTo(Ingredients)
+
+    //* 1:M -> Users - UsersIngredients
+    Users.hasMany(UsersIngredients)
+    UsersIngredients.belongsTo(Users)
+
+    //* 1:M -> Ingredients - UsersIngredients
+    Ingredients.hasMany(UsersIngredients)
+    UsersIngredients.belongsTo(Ingredients)
+
+    //* 1:M -> Recipes - Instructions
+    Recipes.hasMany(Instructions)
+    Instructions.belongsTo(Recipes)
 }
 
 
